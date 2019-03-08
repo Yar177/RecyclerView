311199c7495abdfb96a160a21d6c29a01ef6fde5
Yar <yarsher.dev@gmail.com>
2019-03-08 02:06:09 -0500

    gridView

--- app/src/main/java/com/yar/recyclerview/RecyclerGridAdapter.java
+++ app/src/main/java/com/yar/recyclerview/RecyclerGridAdapter.java
@@ -2,44 +2,54 @@ package com.yar.recyclerview;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class RecyclerGridAdapter extends RecyclerView.Adapter {
+public class RecyclerGridAdapter extends RecyclerView.Adapter<RecyclerGridAdapter.GridViewHolder> {
 
+    private int[] images;
 
+    public RecyclerGridAdapter(int[] images) {
+        this.images = images;
+    }
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+    public GridViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.album_layout, viewGroup, false);
+
+        GridViewHolder gridViewHolder = new GridViewHolder(view);
+
+        return gridViewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+    public void onBindViewHolder(@NonNull GridViewHolder viewHolder, int i) {
+        int imageId = images[i];
+        viewHolder.imageView.setImageResource(imageId);
+        viewHolder.textView.setText("Image: - " + i);
+
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return images.length;
     }
 
     public static class GridViewHolder extends RecyclerView.ViewHolder{
 
         TextView textView;
         ImageView imageView;
-
         public GridViewHolder(@NonNull View itemView) {
             super(itemView);
             imageView = itemView.findViewById(R.id.ivAlbum);
             textView = imageView.findViewById(R.id.tvAmbumTitle);
-
-
         }
-
-
     }
+
+
 }
--- app/src/main/java/com/yar/recyclerview/RecyclerGridView.java
+++ app/src/main/java/com/yar/recyclerview/RecyclerGridView.java
@@ -12,19 +12,12 @@ public class RecyclerGridView extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private int[] images = {
-            R.drawable.p1, R.drawable.p2, R.drawable.p3, R.drawable.p4, R.drawable.p5, R.drawable.p6,
-            R.drawable.p1, R.drawable.p2, R.drawable.p3, R.drawable.p4, R.drawable.p5, R.drawable.p6,
-            R.drawable.p1, R.drawable.p2, R.drawable.p3, R.drawable.p4, R.drawable.p5, R.drawable.p6,
-            R.drawable.p1, R.drawable.p2, R.drawable.p3, R.drawable.p4, R.drawable.p5, R.drawable.p6,
-            R.drawable.p1, R.drawable.p2, R.drawable.p3, R.drawable.p4, R.drawable.p5, R.drawable.p6,
-            R.drawable.p1, R.drawable.p2, R.drawable.p3, R.drawable.p4, R.drawable.p5, R.drawable.p6,
-            R.drawable.p1, R.drawable.p2, R.drawable.p3, R.drawable.p4, R.drawable.p5, R.drawable.p6,
-            R.drawable.p1, R.drawable.p2, R.drawable.p3, R.drawable.p4, R.drawable.p5, R.drawable.p6,
-            R.drawable.p1, R.drawable.p2, R.drawable.p3, R.drawable.p4, R.drawable.p5, R.drawable.p6,
-            R.drawable.p1, R.drawable.p2, R.drawable.p3, R.drawable.p4, R.drawable.p5, R.drawable.p6
+            R.drawable.p2, R.drawable.p1, R.drawable.p2, R.drawable.p1
     };
 
     private RecyclerView.LayoutManager layoutManager;
+
+    private RecyclerGridAdapter recyclerGridAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,8 +30,9 @@ public class RecyclerGridView extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(layoutManager);
 
+        recyclerGridAdapter = new RecyclerGridAdapter(images);
 
-
+        recyclerView.setAdapter(recyclerGridAdapter);
 
 
 
