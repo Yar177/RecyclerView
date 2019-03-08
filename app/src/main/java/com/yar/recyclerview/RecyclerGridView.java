56d169e86810f28fdf0cbc3d046020793b24de7e
Yar <yarsher.dev@gmail.com>
2019-03-08 02:54:45 -0500

    finalize

--- app/src/main/java/com/yar/recyclerview/RecyclerGridView.java
+++ app/src/main/java/com/yar/recyclerview/RecyclerGridView.java
@@ -14,9 +14,12 @@ public class RecyclerGridView extends AppCompatActivity {
     private int[] images = {
             R.drawable.p2, R.drawable.p1, R.drawable.p3, R.drawable.p4,
             R.drawable.p5, R.drawable.p6, R.drawable.p7, R.drawable.p8,
+            R.drawable.p13, R.drawable.p14, R.drawable.p15,
             R.drawable.p2, R.drawable.p1, R.drawable.p3, R.drawable.p4,
+            R.drawable.p13, R.drawable.p14, R.drawable.p15,
             R.drawable.p5, R.drawable.p6, R.drawable.p7, R.drawable.p8,
             R.drawable.p9, R.drawable.p10, R.drawable.p11, R.drawable.p12,
+            R.drawable.p13, R.drawable.p14, R.drawable.p15,
             R.drawable.p2, R.drawable.p1, R.drawable.p3, R.drawable.p4,
             R.drawable.p9, R.drawable.p10, R.drawable.p11, R.drawable.p12,
             R.drawable.p13, R.drawable.p14, R.drawable.p15
